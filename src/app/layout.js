bd087e7000275d7c65552692085e7b1d6516c9ae
shahmeerdevdex <128673394+AliGates915@users.noreply.github.com>
2026-02-25 10:20:03 +0500

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html  data-arp="">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
